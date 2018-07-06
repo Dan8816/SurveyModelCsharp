8ec8e77ab83e3cf2a39fba1d316a3b69d548e485
Dan8816 <dan881631537518@gmail.com>
2018-07-06 16:02:54 -0500

    who keeps track

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Survey.Models;
+
+namespace Survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+        [HttpPost]     
+        [Route("/submit")]    
+        public IActionResult Submit(string Name, string Location, string Language, string Comment)
+        {
+            ViewData["Message"] = "Thanks you for you submitted data!";
+            Ninja thisNinja = new Ninja()
+            {
+                Name = Name,
+                Location = Location,
+                Language = Language,
+                Comment = Comment,
+            };
+            return View(thisNinja);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View("submit");
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Ninjas.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Survey.Models
+{
+    public class Ninja
+    {
+     public string Name {get;set;}
+     public string Location {get;set;}
+     public string Language {get;set;}
+     public string Comment {get;set;}
+    }
+}
